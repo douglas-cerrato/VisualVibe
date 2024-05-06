8fa8773c5fa1442c63640401f70346005b3c3366
douglas-cerrato <douglascerrato2@gmail.com>
2024-05-06 04:59:38 +0000

    working on unique username feature

--- create_username.php
+++ create_username.php
@@ -1,4 +1,7 @@
 <?php
+    // Make DB Connection
+    require_once('DBConnection.php');
+    $conn = getDBConnection();
     // Start the Session
     session_start();
 
@@ -10,6 +13,25 @@
 
     // Unset all session variables
     session_unset();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST['username'];
+        $sql_query = "SELECT * FROM vibeUsers WHERE Username = ?";
+        $stmnt = $conn->prepare($sql_query);
+        $stmnt->bind_param("s", $username);
+        $stmnt->execute();
+        $result = $stmnt->get_result()->fetch_assoc();
+
+        if($result != NULL){
+            $usernameTaken = true;
+            header('Location: create_username.php');
+            exit();
+        }else{
+            $uniqueUsername = true;
+            header('Location: create_username.php');
+            exit();
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -22,11 +44,22 @@
 </head>
 <body>
     <h1>Create Username</h1>
-    <p>Welcome to Visual Vibe, <?php echo $fname; ?>!</p>
+    <p>Welcome to VisualVibe <?php echo $fname;?>!</p>
     <form action="create_username.php" method="post">
         <label for="username">Choose a Username</label>
         <input type="text" id="username" name="username" required>
         <input type="submit" value="Submit">
     </form>
+    <?php
+        if($usernameTaken == true){
+            echo '<p style="color: red;">Username Already Taken.... Try again!</p>';
+        }
+        if($uniqueUsername == true){
+            echo '<p style="color: green;">Username Created!</p>';
+            // Sleep so User can see message
+            sleep(5);
+            die("User created a unique username");
+        }
+    ?>
 </body>
 </html>
